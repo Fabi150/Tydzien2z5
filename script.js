0920d0779df0cea38266cb9714be9af2bece79fb
Radoslaw Fabisiak <radoslawfabisiak@gmail.com>
2017-10-19 00:36:33 +0200

    Adding toggle() method.

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+// Rozwiązanie wg zadania z sylabusa
+
+
+//function Toggler(selector){
+//	
+//	this._obj = document.querySelector(selector);
+//	
+//};
+//
+//Toggler.prototype.getElem = function(){
+//	
+//	return this._obj;
+//	
+//};
+//
+//Toggler.prototype.show = function(){
+//	
+//	this._obj.style.display = "";
+//	
+//};
+//
+//Toggler.prototype.hide = function(){
+//	
+//	this._obj.style.display = "none";
+//	
+//};
+//
+//
+//var elem = new Toggler("#section");
+//var button = document.querySelector("#button");
+//
+//button.addEventListener("click", function() {
+//
+//    if(elem.getElem().style.display == "none") {
+//        elem.show();
+//    } else {
+//        elem.hide();
+//    }
+//
+//}, false);
+
+
+
+// bardziej zoptymalizowane rozwiązanie
+
 function Toggler(selector){
 	
 	this._obj = document.querySelector(selector);
@@ -22,16 +67,37 @@ Toggler.prototype.hide = function(){
 	
 };
 
+Toggler.prototype.toggle = function(){
+	
+	 if(this._obj.style.display == "none") {
+        this.show();
+    } else {
+        this.hide();
+    };
+	
+};
+
 
 var elem = new Toggler("#section");
 var button = document.querySelector("#button");
 
 button.addEventListener("click", function() {
 
-    if(elem.getElem().style.display == "none") {
-        elem.show();
-    } else {
-        elem.hide();
-    }
+    elem.toggle();
 
 }, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
